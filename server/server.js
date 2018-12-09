eebb47911c54d8ae5a0c8c3a5b9897083c13e3b7
Ashutosh Ranjan Jha <arjha@Ashutoshs-MacBook-Air.local>
2018-12-09 18:31:04 +0530

    steup for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
+var port=process.env.port||3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -57,8 +59,8 @@ app.get('/todos/:id', (req, res) => {
     }
 });
 
-app.listen('3000', () => {
-    console.log('app is started at port 3000');
+app.listen(port, () => {
+    console.log(`app is started at port ${port}`);
 });
 
 
