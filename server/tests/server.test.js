08b9ee1cca056561cb0b2e1b844c656241984093
Ashutosh Ranjan Jha <arjha@Ashutoshs-MacBook-Air.local>
2018-12-09 16:20:38 +0530

    test cases for /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [{
+    _id:new ObjectID(),
     text: 'first test todo'
 }, {
+    _id:new ObjectID(),
     text: 'second test todo'
 }];
 beforeEach((done) => {
@@ -65,5 +68,31 @@ describe('GET /todos', () => {
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done);
+    });
+});
+
+describe('GET /todos/:Id',()=>{
+    it('Should return todo doc',(done)=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.text).toBe(todos[0].text);
+        })
+        .end(done)
+    });
+
+    it('Should retuen 404 if todo not found',(done)=>{
+        request(app)
+        .get(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done)
+    });
+
+    it('Should retuen 404 non object id',(done)=>{
+        request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done)
     });
 });
