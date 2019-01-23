c62856bab202ae75e965e38ec52fdbe75f6e32ee
Ashutosh Ranjan Jha <arjha@Ashutoshs-MacBook-Air.local>
2019-01-23 17:47:01 +0530

    auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -48,6 +48,22 @@ userSchema.methods.generateAuthToken = function () {
     });
 
 }
+userSchema.statics.findByToken = function (token) {
+    let User = this;
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (error) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
 
 var User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const { config } = require('./envConfig/envConfig');
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const options = {
     key: fs.readFileSync(__dirname + '/server.key'),
@@ -54,10 +55,14 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
-        res.header('x-auth',token).send(user);
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 //GET /todos/:Id
